4c01598f9039fed84855831647cd06743d1a7fd5
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-08-12 12:53:19 -0400

    installed express mongoose. Updated model files and exported them so they can be used properly

---
+++ controllers/thoughtsController.js
@@ -0,0 +1,8 @@
+const { User, Thought } = require('../models');
+
+const thoughtsController = {
+ getAllThoughts (req,res) {
+  Thought.find({})
+  .populate({path: 'reactions', select: '-__v'})
+ }
+}
