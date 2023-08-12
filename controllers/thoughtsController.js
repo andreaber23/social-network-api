556eb0c08afd8427881f2749d8f1d0c356e8761a
AndreaB <128647787+andreaber23@users.noreply.github.com>
2023-08-12 16:09:02 -0400

    userRoute file completed with code to get, update, and delete actions

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,8 +1,12 @@
 const { User, Thought } = require('../models');
 
 const thoughtsController = {
- getAllThoughts (req,res) {
-  Thought.find({})
-  .populate({path: 'reactions', select: '-__v'})
+    getAllThoughts (req,res) {
+        Thought.find({})
+        .populate({
+            path: 'reactions', 
+            select: '-__v'})
+        .select
+            
  }
 }
